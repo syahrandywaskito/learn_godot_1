8562045cbfb479798b269226546892b45a1e84ce
syahrandywaskito <syahrandywaskito666@gmail.com>
2023-10-19 13:24:34 +0700

    Update : Basic side-scroller movement

---
+++ Scenes/Player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+# visible in inspector editor
+@export var speed : float = 300.0
+@export var jump_velocity : float = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# for value debugging 
+func _ready():
+#	print("Nilai Gravitasi : " + str(gravity))
+	pass
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
